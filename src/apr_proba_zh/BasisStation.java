6b1a62b3d086f621752928c0eb83bf46d45eebed
Oliver Kocsis <okocsis@codingsans.com>
2017-05-03 14:28:45 +0200

    observer

--- src/apr_proba_zh/BasisStation.java
+++ src/apr_proba_zh/BasisStation.java
@@ -1,7 +1,39 @@
 package apr_proba_zh;
 
-public class BasisStation extends Cell{
+import java.util.Observable;
+
+public class BasisStation extends Cell {
+	
+	class NameRadio extends Observable {
+		NameRadio(BasisStation owner) {
+			this.owner = owner;
+		}
+		BasisStation owner;
+		public BasisStation getOwner() {
+			return this.owner;
+		}
+		void setChangedPublic() {
+			super.setChanged();
+		}
+		public void notifyObservers(String changedName) {
+			super.notifyObservers(changedName);
+		}
+	}
+	
+	NameRadio nameRadio = new NameRadio(this);
 	String name;
+	
+	public String getName() {
+		return name;
+		
+	}
+
+	public void setName(String name) {
+		this.name = name;
+		this.nameRadio.setChangedPublic();
+		this.nameRadio.notifyObservers(name);
+	}
+
 	
 	public BasisStation(double r, double c, String name) {
 		super(r, c);
--- src/apr_proba_zh/Main.java
+++ src/apr_proba_zh/Main.java
@@ -1,5 +1,10 @@
 package apr_proba_zh;
 
+import java.util.Observable;
+import java.util.Observer;
+
+import apr_proba_zh.BasisStation.NameRadio;
+
 public class Main {
 	
 	public static Matrix basises = new Matrix();
@@ -15,16 +20,48 @@ public class Main {
 		Matrix matrix = new Matrix();
 //		Matrix basises  = new Matrix();
 		
-		Cell b1 = new BasisStation(0.0, 0.0, "egyes");
-		Cell b2 = new BasisStation(1.0, 6.0, "kettes");
-		Cell b3 = new BasisStation(4.0, 4.0, "harmas");
-		Cell b4 = new BasisStation(7.0, 1.0, "negyes");
+		BasisStation b1 = new BasisStation(0.0, 0.0, "egyes");
+		BasisStation b2 = new BasisStation(1.0, 6.0, "kettes");
+		BasisStation b3 = new BasisStation(4.0, 4.0, "harmas");
+		BasisStation b4 = new BasisStation(7.0, 1.0, "negyes");
 		
 //		basises.cellList.add(b1);
 //		basises.cellList.add(b2);
 //		basises.cellList.add(b3);
 //		basises.cellList.add(b4);
 		basisesSet(b1, b2, b3, b4);
+		
+		Observer observer1 = new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				if (o instanceof BasisStation.NameRadio && arg instanceof String) {
+					NameRadio radio = (NameRadio)o;
+					BasisStation station = radio.owner;
+					String name = (String)arg;
+					System.out.println("Observer1 \n" + station.toString() + " sais: name changed to: " + name); 
+				}
+				
+			}
+		};
+		Observer observer2 = new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				if (o instanceof BasisStation.NameRadio && arg instanceof String) {
+					NameRadio radio = (NameRadio)o;
+					BasisStation station = radio.owner;
+					String name = (String)arg;
+					System.out.println("Observer2 \n" + station.toString() + " sais: name changed to: " + name); 
+				}
+				
+			}
+		};
+		b1.nameRadio.addObserver(observer1);
+		b1.nameRadio.addObserver(observer2);
+		b1.setName("cica");
+		
+		b1.nameRadio.deleteObserver(observer1);
+		b1.nameRadio.deleteObserver(observer2);
+		b1.setName("cica");
 		
 		
 		for (int i = 0; i<8; i++) {
@@ -40,8 +77,6 @@ public class Main {
 				}
 				matrix.cellList.add(actcell);
 				System.out.println(actcell.getRow() + ", " + actcell.getCol() + ", " + actcell.basis.name);
-
-				
 			}
 		}
 		
@@ -53,10 +88,6 @@ public class Main {
 			Thread threadx = new Thread(mobilx);
 			threadx.start();	
 		}
-		
-
-
-		
 
 	}
 
